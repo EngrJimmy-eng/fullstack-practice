ba95bd9f8e2778259b82a3a3540358628317cf90
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-04-08 09:15:51 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,30 +19,9 @@ resource "aws_instance" "app" {
   }
 }
 
-resource "aws_iam_role" "ssm_role" {
-  name = "ssm-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
 
-resource "aws_iam_role_policy_attachment" "ssm_attach" {
-  role       = aws_iam_role.ssm_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
 
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "ssm-instance-profile-1"
-  role = aws_iam_role.ssm_role.name
-}
 
 terraform {
   backend "s3" {
