b4786d4d72425001b009fcadb4bfefa454d9bf83
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-04-08 13:06:24 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,24 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# ✅ DATA SOURCE: Fetch latest Amazon Linux 2 AMI
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] # Amazon official account
+}
+
+# ✅ EC2 INSTANCE
 resource "aws_instance" "app" {
   ami           = data.aws_ami.latest_amazon_linux.id
   instance_type = "t2.micro"
@@ -27,6 +45,5 @@ terraform {
     encrypt        = true
   }
 }
-
 
 
